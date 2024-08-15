be67688acea59a5ca5f161f545a294be8bc8d989
Satendra Sahani <satendra@clickinpedia.com>
2024-08-15 13:36:08 +0530

    Linked list with first instertion and last insertion

---
+++ linked_list/LinkedList.java
@@ -0,0 +1,63 @@
+/**
+ * LinkedList
+ */
+public class LinkedList {
+
+    public class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() {
+        Node temp = head;
+        if (temp == null) {
+            System.out.println("ll is empty");
+            return;
+        }
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList l1 = new LinkedList();
+        l1.addFirst(10);
+        l1.addFirst(12);
+        l1.addFirst(19);
+        l1.addLast(13);
+        l1.print();
+
+    }
+}
